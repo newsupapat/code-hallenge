f94f48e55f82c1e5f509fa6206b767a01d8f9109
newsupapat <supapat.ne@gmail.com>
2019-07-16 12:36:40 +0700

    init->test

--- index.js
+++ index.js
@@ -10,15 +10,33 @@ app.get('/', (req, res) => {
   res.send('Hello')
 })
 app.post('/compilecode', (req, res) => {
-  const sourcecode = `print("Hell0 W0rld!")`
-  console.log(req.body.code)
-  let resultPromise = c.runSource(req.body.code)
+  // const sourcecode = `print("Hell0 W0rld!")`
+  const { code, lang } = req.body
+  switch (lang) {
+    case 'c':
+      let resultPromise = c.runSource(code)
+      break
+    case 'cpp':
+      let resultPromise = cpp.runSource(code)
+      break
+    case 'node':
+      let resultPromise = node.runSource(code)
+      break
+    case 'python':
+      let resultPromise = python.runSource(code)
+      break
+    default:
+      res.send('error')
+      break
+  }
   resultPromise
     .then(result => {
       console.log(result)
+      res.send(result)
     })
     .catch(err => {
       console.log(err)
+      res.send(err)
     })
 })
 app.listen(PORT, () => {
